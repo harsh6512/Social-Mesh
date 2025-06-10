4ab79528fc48d8c1cc2dd179a9edeb2671164e30
harsh6512 <harshkumar1987ynr@gmail.com>
2025-06-10 13:37:29 +0530

    added utils folder

---
+++ apps/api/src/utils/asyncHandler.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction, RequestHandler } from "express";
+
+type AsyncHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<any>;
+
+const asyncHandler = (requestHandler: AsyncHandler): RequestHandler => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch(next);
+  };
+};
+
+export { asyncHandler };
+

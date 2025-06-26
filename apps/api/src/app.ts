72f063ab0d43390e8be57458e90fe83082f96360
harsh6512 <harshkumar1987ynr@gmail.com>
2025-06-26 17:52:30 +0530

    removed some testing code

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -23,12 +23,4 @@ app.use("/api/v1/users", userRouter)
 app.use("/api/v1/profiles", profileRouter)
 app.use(errorHandler)
 
-app.get('/', (req, res) => {
-  res.send(`
-    <h1>Welcome to Social Mesh</h1>
-    <a href="/api/v1/auth/google/signin">Sign in with Google</a>
-    <a href="/api/v1/auth/google/signup">Sign up with Google</a>
-  `);
-});
-
 export { app };

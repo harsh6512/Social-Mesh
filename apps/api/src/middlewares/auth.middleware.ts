4e6c6f43a87184a3aa6592df85e40a1dea487bd0
harsh6512 <harshkumar1987ynr@gmail.com>
2025-07-08 11:11:27 +0530

    feat: include profile data in req.user during JWT verification

--- apps/api/src/middlewares/auth.middleware.ts
+++ apps/api/src/middlewares/auth.middleware.ts
@@ -1,39 +1,37 @@
-import { ApiError } from "../utils/ApiError.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
-import jwt from "jsonwebtoken";
-import { prisma } from "../db/index.js"
-import { ENV } from "../constants/env.js";
-import { Request,Response,NextFunction } from "express"
-import { sanitizeUser } from "../utils/sanitizeUser.js";
+import { ApiError } from '../utils/ApiError.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import jwt from 'jsonwebtoken';
+import { prisma } from '../db/index.js';
+import { ENV } from '../constants/env.js';
+import { Request, Response, NextFunction } from 'express';
+import { sanitizeUser } from '../utils/sanitizeUser.js';
 
-export const verifyJWT = asyncHandler(async (req:Request, _:Response, next:NextFunction) => {
+export const verifyJWT = asyncHandler(async (req: Request, _: Response, next: NextFunction) => {
     try {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
-        if (!token) {
-            throw new ApiError(401, "Unathorized request")
-        }
+        if (!token) throw new ApiError(401, "Unathorized request")
 
         const decoded = jwt.verify(token, ENV.ACCESS_TOKEN_SECRET);
-        if (typeof decoded !== "object" || !("id" in decoded)) {
-            throw new ApiError(401, "Invalid token payload");
-        }
+        if (typeof decoded !== "object" || !("id" in decoded)) throw new ApiError(401, "Invalid token payload");
 
         const decodedToken = decoded as jwt.JwtPayload;
 
         const user = await prisma.user.findUnique({
             where: {
                 id: decodedToken?.id
+            },
+            include: {
+                profile: true,
             }
         })
+        if (!user) throw new ApiError(401, "Invalid Access Token")
 
-        if (!user) {
-            throw new ApiError(401, "Invalid Access Token")
-        }
-        const safeUser=sanitizeUser(user)
+        if (!user.profile?.id) throw new ApiError(403, "Complete your profile to perform this action")
+
+        const safeUser = sanitizeUser(user)
         req.user = safeUser;
         next();
     } catch (error: unknown) {
-
         if (error instanceof Error) {
             throw new ApiError(401, error.message);
         }

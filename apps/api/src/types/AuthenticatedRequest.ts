f44c619665809a6f5d913233176cf4987274683e
harsh6512 <harshkumar1987ynr@gmail.com>
2025-07-23 17:16:02 +0530

    chore(types): add deviceId to AuthenticatedRequest user type

--- apps/api/src/types/AuthenticatedRequest.ts
+++ apps/api/src/types/AuthenticatedRequest.ts
@@ -4,5 +4,6 @@ import { User, Profile } from '../generated/prisma/index.js';
 export interface AuthenticatedRequest extends Request {
   user: Omit<User, 'password' | 'refreshToken'> & {
     profile: Profile;
+    deviceId:string;
   };
 }

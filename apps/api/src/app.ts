0bbfdaf95f07e6dc2e70dcabcd595dedfae56e8f
harsh6512 <harshkumar1987ynr@gmail.com>
2025-07-26 12:04:11 +0530

    chore: add helmet middleware for basic security headers

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -4,13 +4,16 @@ import passport from 'passport'
 import './lib/passport.js'
 import { errorHandler } from './middlewares/errorHandler.middleware.js'
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+
 const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,    
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
+app.use(helmet());
 app.use(cookieParser());
 app.use(passport.initialize())
 
@@ -21,14 +24,16 @@ import followRouter from './routes/follow.routes.js';
 import postRouter from './routes/post.routes.js';
 import likeRouter from './routes/like.routes.js';
 import commentRouter from './routes/comment.routes.js';
+import notificationRouter from './routes/notification.route.js'
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/profiles', profileRouter)
-app.use('/api/v1/connection',followRouter)
-app.use('/api/v1/posts',postRouter)
-app.use('/api/v1/likes',likeRouter)
-app.use('/api/v1/comments',commentRouter)
+app.use('/api/v1/connection', followRouter)
+app.use('/api/v1/posts', postRouter)
+app.use('/api/v1/likes', likeRouter)
+app.use('/api/v1/comments', commentRouter)
+app.use('/api/v1/notification', notificationRouter)
 app.use(errorHandler)
 
 export { app };

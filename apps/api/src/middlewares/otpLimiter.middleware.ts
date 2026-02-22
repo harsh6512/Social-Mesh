3f38e54e21a4165cb0e2d7b8a6a35b082036d2df
harsh6512 <harshkumar1987ynr@gmail.com>
2026-02-22 16:32:16 +0530

    refactor(auth): update OTP rate limiter to use ApiResponse format

--- apps/api/src/middlewares/otpLimiter.middleware.ts
+++ apps/api/src/middlewares/otpLimiter.middleware.ts
@@ -1,12 +1,21 @@
 import rateLimit from "express-rate-limit";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 export const otpLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 3, // Limit each IP to 3 requests per windowMs
-  message: {
-    status: 429,
-    error: "Too many OTP requests. Please try again after 15 minutes"
-  },
+  max: 3,
   standardHeaders: true,
   legacyHeaders: false,
+
+  handler: (req, res) => {
+    return res
+      .status(429)
+      .json(
+        new ApiResponse(
+          429,
+          null,
+          "Too many OTP verification attempts. Please try again after 15 minutes."
+        )
+      );
+  },
 });
